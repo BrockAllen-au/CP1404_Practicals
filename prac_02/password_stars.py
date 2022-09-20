81df6364e325c6fba12eb1d02f5a8768411c4b1a
b_rock_solid <brock.allen61@gmail.com>
2022-09-20 16:08:07 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,25 @@
 """CP1404 Password Stars program"""
 
-min_password_length = 10
-password = input("Password: ")
-while len(password) < min_password_length:
-    print("Password too short")
-    password = input("Password:")
-print("*" * len(password))
+
+def main():
+    """Password Stars Program"""
+    min_password_length = 10
+    password = get_password(min_password_length)
+    print_password_asterisks(password)
+
+
+def print_password_asterisks(password):
+    """Print the password length as asterisks"""
+    print("*" * len(password))
+
+
+def get_password(min_password_length):
+    """Get a valid password with a minimum required length"""
+    password = input("Password: ")
+    while len(password) < min_password_length:
+        print("Password too short")
+        password = input("Password:")
+    return password
+
+
+main()
