6286daedfae54b37ed1135eadb4b4d56de275e3f
b_rock_solid <brock.allen61@gmail.com>
2022-09-21 12:20:16 +1000

    Refactor score program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,14 +1,31 @@
 """
-CP1404/CP5632 - Practical
-Broken program to determine score status
+CP1404/CP5632 - Practical_02 Score program refactoring
 """
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    """Score results calculator program"""
+    score = float(input("Enter score: "))
+    result = calculate_result(score)
+    print(f"Result: {result}")
+    random_score = random.randint(0, 100)
+    print(f"Random score: {random_score}")
+    result = calculate_result(random_score)
+    print(f"Result: {result}")
+
+
+def calculate_result(score):
+    """Calculate the result based on the score provided"""
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
