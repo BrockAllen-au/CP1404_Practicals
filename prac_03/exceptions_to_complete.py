ebf706215234b668bb5bdd439edda400e9961d25
b_rock_solid <brock.allen61@gmail.com>
2022-09-26 15:20:35 +1000

    Refactored to use correct exception

--- prac_03/exceptions_to_complete.py
+++ prac_03/exceptions_to_complete.py
@@ -7,7 +7,7 @@ is_finished = False
 while not is_finished:
     try:
         result = int(input("Enter a valid integer: "))
-        # TODO: this line
-    except:  # TODO - add the exception you want to catch after except
+        is_finished = True  # Exit loop as valid integer has been entered
+    except ValueError:  # Exception for ValueError to handle non-integer input
         print("Please enter a valid integer.")
 print("Valid result is:", result)
