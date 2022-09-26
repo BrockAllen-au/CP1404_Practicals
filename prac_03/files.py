e5176e6ad0fd9e060d16a73d5ff822d7e7de6f85
b_rock_solid <brock.allen61@gmail.com>
2022-09-26 16:01:30 +1000

    Read name from file

--- prac_03/files.py
+++ prac_03/files.py
@@ -19,3 +19,9 @@ name = input("What is your name? ").title()
 out_file = open("name.txt", 'w')
 print(name, file=out_file)
 out_file.close()
+
+# 2
+in_file = open("name.txt", 'r')
+name = in_file.readline()
+print(f"Your name is {name}")
+in_file.close()
