92d03a152c28bd103f48fc048a4a2f6d54795438
b-rock-solid <brock.allen61@gmail.com>
2022-09-26 18:58:58 +1000

    Validate minimum 'normal'characters are entered to password

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -48,7 +48,9 @@ def is_valid_password(password):
         if char in SPECIAL_CHARACTERS:
             count_special += 1
 
-    # TODO: if any of the 'normal' counts are zero, return False
+    # If any of the 'normal' counts are zero, return False
+    if count_lower == 0 or count_upper == 0 or count_digit == 0:
+        return False
 
     # TODO: if special characters are required, then check the count of those
     # and return False if it's zero
