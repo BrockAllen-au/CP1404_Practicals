48a3dc063aa446aa3e10e28b0cc0d02e40f5c88f
b_rock_solid <brock.allen61@gmail.com>
2022-10-04 15:01:52 +1000

    Add requested expressions to program

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -39,3 +39,21 @@ print(f'"3" in numbers:\n{"3" in numbers}')
 # numbers + [6, 5, 3]
 print(f'numbers + [6, 5, 3]:\n{numbers + [6, 5, 3]}')
 # Result - numbers list will have 6, 5, 3 on the end of the list
+
+print(f"\nInitial numbers list:\n{numbers}")
+
+# Change the first element of numbers to "ten" (the string, not the number 10)
+numbers[0] = "ten"
+print(f"1. {numbers}")
+
+# Change the last element of numbers to 1
+numbers[-1] = 1
+print(f"2. {numbers}")
+
+# Get all the elements from numbers except the first two (slice)
+# numbers[2:]
+print(f"3. {numbers[2:]}")
+
+# Check if 9 is an element of numbers
+# 9 in numbers
+print(f"4. Is 9 in numbers? {9 in numbers}")
