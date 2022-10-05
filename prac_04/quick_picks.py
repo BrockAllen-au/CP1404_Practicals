04589d0980f827df43384a96de882cf146468ba3
BrockAllen-au <brock.allen61@gmail.com>
2022-10-05 16:29:42 +1000

    Add quick pick generator function

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -12,11 +12,29 @@ from random import randrange
 
 START_RANGE = 1
 END_RANGE = 46  # Randrange doesn't include final number in range
+NUMBERS_TO_GENERATE = 6  # How many numbers to pick on each line
 
 
 def main():
     """Lottery quick pick generator program."""
     number_of_quick_picks = get_valid_number("How many quick picks? ")
+    generate_quick_picks(number_of_quick_picks)
+
+
+def generate_quick_picks(number_of_quick_picks):
+    """Generate n number of lottery quick picks"""
+    for i in range(number_of_quick_picks):
+        picks = []
+        for j in range(NUMBERS_TO_GENERATE):
+            number = randrange(START_RANGE, END_RANGE)
+            while number in picks:
+                # If number already in list, generate a new one
+                number = randrange(START_RANGE, END_RANGE)
+            picks.append(number)
+        picks.sort()
+        for number in picks:
+            print(f"{number:2}", end=" ")
+        print()
 
 
 def get_valid_number(prompt):
