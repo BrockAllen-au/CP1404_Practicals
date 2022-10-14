165908a6e8e3d6c1185c9b5a3b25fec5639cf238
BrockAllen-au <brock.allen61@gmail.com>
2022-10-14 15:38:52 +1000

    Display word counts with variable width based on longest word

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,7 +1,7 @@
 """
 Word Occurrences
 Estimate: 30 minutes
-Actual:
+Actual: 38 minutes
 """
 
 text = input("Text: ")
@@ -11,5 +11,9 @@ for word in text:
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
 words_alphabetical = sorted(word_to_count.items())
+width = 0
 for word in words_alphabetical:
-    print(f"{word[0]} : {word[1]}")
+    if len(word[0]) > width:
+        width = len(word[0])
+for word in words_alphabetical:
+    print(f"{word[0]:{width}} : {word[1]}")
