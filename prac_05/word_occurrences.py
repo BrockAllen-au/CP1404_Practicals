a30234a9f47a32aad1ad07688139478240e12512
BrockAllen-au <brock.allen61@gmail.com>
2022-10-14 15:28:35 +1000

    Display word counts in alphabetical order

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -4,3 +4,12 @@ Estimate: 30 minutes
 Actual:
 """
 
+text = input("Text: ")
+text = text.split(" ")
+word_to_count = {}
+for word in text:
+    word_to_count[word] = word_to_count.get(word, 0) + 1
+
+words_alphabetical = sorted(word_to_count.items())
+for word in words_alphabetical:
+    print(f"{word[0]} : {word[1]}")
