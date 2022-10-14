bdaa4155da081277430e5d8f927b761948f1522d
BrockAllen-au <brock.allen61@gmail.com>
2022-10-14 14:57:01 +1000

    Refactor to make key lookup case-independent

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -7,10 +7,11 @@ COLOUR_TO_CODE = {"AbsoluteZero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue
                   "Alizarin Crimson": "#e32636", "Amaranth": "#e52b50", "Amber": "#ffbf00", "Amethyst": "#9966cc",
                   "AntiqueWhite": "#faebd7", "Apricot": "#fbceb1", "Aqua": "#00ffff"}
 
-colour_name = input("Enter colour name: ")
+colour_name = input("Enter colour name: ").title()
 while colour_name != "":
     try:
+        code = {colour.title(): code for colour, code in COLOUR_TO_CODE.items()}
         print(colour_name, "has colour code: ", code[colour_name])
     except KeyError:
         print("Invalid colour entered.")
-    colour_name = input("Enter colour name: ")
+    colour_name = input("Enter colour name: ").title()
