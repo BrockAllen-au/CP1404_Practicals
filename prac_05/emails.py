15614439f91aff5bec7626873e658d57ce5e5757
BrockAllen-au <brock.allen@my.jcu.edu.au>
2022-10-15 07:33:19 +1000

    Display emails and names stored in dictionary

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,7 +6,14 @@ Email and name extractor program
 
 def main():
     """Create a dictionary of emails to names."""
-    pass
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = extract_name(email)
+        email_to_name[email] = name
+        email = input("Email: ")
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
 
 
 def extract_name(email):
@@ -17,3 +24,6 @@ def extract_name(email):
     if name_confirmation.upper() != "Y" and name_confirmation != "":
         name = input("Name: ").title()
     return name
+
+
+main()
