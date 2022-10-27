767326f6d4c30f508bc36335e10e4841910f8b08
BrockAllen-au <brock.allen@my.jcu.edu.au>
2022-10-27 18:45:28 +1000

    Complete guitar class testing

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -2,6 +2,7 @@
 CP1404/CP5632 Practical - Guitar Class.
 Expected time to write class: 20mins
 Start time: 6:30pm
+End time: 6:45pm
 """
 CURRENT_YEAR = 2022
 
---
+++ prac_06/guitar_test.py
@@ -0,0 +1,9 @@
+from guitar import Guitar
+
+my_guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
+another_guitar = Guitar("Another Guitar", 2013, 1015)
+
+print(f"{my_guitar.name} get_age() - Expected 100. Got", my_guitar.get_age())
+print(f"{another_guitar.name} get_age() - Expected 9. Got", another_guitar.get_age())
+print(f"{my_guitar.name} is_vintage() - Expected True. Got", my_guitar.is_vintage())
+print(f"{another_guitar.name} is vintage() - Expected False. Got", another_guitar.is_vintage())
