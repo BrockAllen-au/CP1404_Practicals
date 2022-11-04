14b1b02a2a7b0f9ef99ac571a75cf1fb88275ccb
BrockAllen-au <brock.allen61@gmail.com>
2022-11-04 11:11:31 +1000

    Add ability to save to file

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -9,11 +9,20 @@ FILENAME = "guitars.csv"
 def main():
     """Main guitar display program."""
     guitars = []
-    load_guitars(guitars)
+    load_guitars(guitars, FILENAME)
     print("My guitars!")
     get_guitars(guitars)
     guitars.sort()
     display_guitars(guitars)
+    save_guitars(guitars, FILENAME)
+
+
+def save_guitars(guitars, filename):
+    """Save current list of guitars to csv file."""
+    out_file = open(filename, "w")
+    for guitar in guitars:
+        print(guitar.name, guitar.year, guitar.cost, file=out_file, sep=",")
+    out_file.close()
 
 
 def get_guitars(guitars):
@@ -35,9 +44,9 @@ def display_guitars(guitars):
         print(f"Guitar {i}: {guitar.name:>{width}} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
 
-def load_guitars(guitars):
+def load_guitars(guitars, filename):
     """Loads guitar information from a csv file."""
-    in_file = open(FILENAME, "r")
+    in_file = open(filename, "r")
     for line in in_file:
         parts = line.strip().split(",")
         guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
