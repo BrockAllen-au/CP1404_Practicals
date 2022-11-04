4d30f6577ea9213b37f03813f0988de69fb8b7ac
BrockAllen-au <brock.allen61@gmail.com>
2022-11-04 11:00:08 +1000

    Add ability for user to add their guitars

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -9,14 +9,34 @@ FILENAME = "guitars.csv"
 def main():
     """Main guitar display program."""
     guitars = []
-    process_guitars(guitars)
+    load_guitars(guitars)
+    print("My guitars!")
+    get_guitars(guitars)
     guitars.sort()
-    for guitar in guitars:
-        print(guitar)
+    display_guitars(guitars)
 
 
-def process_guitars(guitars):
-    """Processes guitar information from a csv file."""
+def get_guitars(guitars):
+    """Prompts user for new guitars until name is blank."""
+    guitar_name = input("Name: ")
+    while guitar_name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        new_guitar = Guitar(guitar_name, year, cost)
+        guitars.append(new_guitar)
+        print(new_guitar, "added.\n")
+        guitar_name = input("Name: ")
+
+
+def display_guitars(guitars):
+    width = max([len(guitar.name) for guitar in guitars])
+    for i, guitar in enumerate(guitars, start=1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>{width}} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+def load_guitars(guitars):
+    """Loads guitar information from a csv file."""
     in_file = open(FILENAME, "r")
     for line in in_file:
         parts = line.strip().split(",")
