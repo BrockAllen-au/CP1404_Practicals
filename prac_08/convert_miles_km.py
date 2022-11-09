64347aa586735b6eb612eacf7a000fee6260e476
BrockAllen-au <brock.allen@my.jcu.edu.au>
2022-11-09 12:58:53 +1000

    Create overall box layout of program

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,3 +1,21 @@
 """
 Convert miles to km kivy program
 """
+
+from kivy.app import App
+from kivy.lang import Builder
+
+__author__ = "Brock Allen"
+
+
+class MilesToKmConverterApp(App):
+    """Miles To KM Converter App is used for converting Miles to Kilometers"""
+
+    def build(self):
+        """Build the Kivy app for converting Miles to KM"""
+        self.title = "Miles to KM Converter"
+        self.root = Builder.load_file("convert_miles_km.kv")
+        return self.root
+
+
+MilesToKmConverterApp().run()
