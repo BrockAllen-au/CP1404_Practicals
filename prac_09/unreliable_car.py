fe426a4c13b5ad431621bb20f92e1db7448f61ad
BrockAllen-au <brock.allen@my.jcu.edu.au>
2022-11-18 17:46:59 +1000

    Refactor class drive method

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -2,11 +2,13 @@
 CP1404/CP5632 Practical
 Unreliable Car class
 """
+import random
 from car import Car
 
 
 class UnreliableCar(Car):
     """Specialised version of a Car that has a chance to not drive."""
+
     def __init__(self, name, fuel, reliability):
         super().__init__(name, fuel)
         self.reliability = reliability
@@ -14,3 +16,15 @@ class UnreliableCar(Car):
     def __str__(self):
         return f"{super().__str__()}, reliability: {self.reliability}"
 
+    def drive(self, distance):
+        """Drive only if distance provided is less than the reliability provided."""
+        reliability_test = random.randint(0, 100)
+        if reliability_test > self.reliability:
+            distance = 0
+        elif distance > self.fuel:
+            distance = self.fuel
+            self.fuel = 0
+        else:
+            self.fuel -= distance
+        self.odometer += distance
+        return distance
--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -2,3 +2,9 @@
 
 from unreliable_car import UnreliableCar
 
+old_trusty = UnreliableCar("Old Trusty", 100, 50)
+print(old_trusty)
+
+old_trusty.drive(60)
+
+print(old_trusty)
