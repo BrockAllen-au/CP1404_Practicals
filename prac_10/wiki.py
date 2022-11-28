47f918fcc4c3e29cf8562c1fb98a0a3a07213a87
BrockAllen-au <brock.allen61@gmail.com>
2022-11-28 12:28:16 +1000

    Add page search program

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -3,3 +3,18 @@ CP1404 Practical - Wikipedia API
 """
 
 import wikipedia
+
+page_search = input("Enter your search: ")
+while page_search != "":
+    try:
+        page = wikipedia.page(page_search, auto_suggest=False)
+        print(page.title)
+        print()
+        print("Summary:")
+        print(page.summary)
+        print()
+        print(page.url)
+    except wikipedia.exceptions.DisambiguationError as e:
+        print("This could refer to multiple pages, please try something more specific")
+        print(e.options)
+    page_search = input("Enter your search: ")
